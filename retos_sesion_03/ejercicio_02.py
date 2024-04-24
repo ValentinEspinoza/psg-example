be97be288c22b9e3081c8f404edc18d5a466c24d
Valentin Espinoza <valentinespinoza.ro@gmail.com>
2024-04-23 22:17:57 -0400

    Dialogo

---
+++ retos_sesion_03/ejercicio_02.py
@@ -0,0 +1,14 @@
+print ("Diálogo:")
+print ("Luis: Hola Marcos, ¿como estas?", '\n'
+"Marcos: Que tal Luis, yo todo tranqui gracias, ¿tu como estas?", '\n'
+"Luis: yo muy bien algo resfriado por este clima cambiante", '\n'
+"Marcos: es verdad es inesperado el clima durante el día, llueve, hace sol, luego hace viento ufff", '\n'
+"Luis: Totalmente cierto, es mejor tener de todo en la mochila y estar prevenidos", '\n'
+"Marcos: Dale, que tal ¿viste el nuevo restaurante de comida rápida?", '\n'
+"Luis: Algo asi, dicen que sirven las mejores hamburguesas de la ciudad, me da curiosidad", '\n'
+"Marcos: Vamos!! yo invito esta vez, ¿te animas?", '\n'
+"Luis: Super, claro me apunto", '\n'
+"Marcos: Buenisimo, mientras vamos cuentame como va el trabajo", '\n'
+"Luis: Ok, aprovechando pedire hasta para llevar jaja", '\n'
+"Marcos: Jaja ok, pero la siguiente te toca invitar eh", '\n'
+"Luis: jaja ok ok", '\n')
