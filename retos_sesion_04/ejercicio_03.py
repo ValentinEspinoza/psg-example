26785a3b42eb95335db35299a774a25fd9ad3989
Valentin Espinoza <valentinespinoza.ro@gmail.com>
2024-04-26 01:02:49 -0400

    Reglas de juego Conecta 4

---
+++ retos_sesion_04/ejercicio_03.py
@@ -0,0 +1,8 @@
+# Reglas de juego CONECTA 4
+print ("1. Se arma el tablero vertical que consiste de 7 filas y 6 columnas")
+print ("2. Cada jugador posee 21 fichas identicas de un color diferente")
+print ("3. Se Debes elegir al jugador que va a empezar")
+print ("4. El jugador coloca una de sus fichas según el color elegido al principio de la partida, en una de las columnas de su elección. Esta ficha cae entonces al fondo de la columna")
+print ("5. El siguiente jugador también pone su ficha en la columna que elija para que también caiga al fondo de la columna")
+print ("6. El juego continúa así hasta que un jugador consigue alinear 4 fichas seguidas en vertical, horizontal o diagonal")
+print ("7. El jugador que consiga alinear 4 fichas de su color elegido es el ganador")
