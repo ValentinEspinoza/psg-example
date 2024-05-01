88f3f1b5ff863ce32887cdc09209a0959fc4b91f
Valentin Espinoza <valentinespinoza.ro@gmail.com>
2024-04-30 23:36:03 -0400

    Reto ejercicio 2

---
+++ retos_sesion_05/ejercicio_02.py
@@ -0,0 +1,13 @@
+# Conversión Temperatura grados Celsius a grados Fahrenheit
+
+temp_1 = 30
+temp_2 = -273.99
+temp_3 = 100
+
+temp_1_f = (temp_1 * 9 / 5) + 32
+temp_2_f = (temp_2 * 9 / 5) + 32
+temp_3_f = (temp_3 * 9 / 5) + 32
+
+print(temp_1, "°C es igual a", temp_1_f, "°F")
+print(temp_2, "°C es igual a", temp_2_f, "°F")
+print(temp_3, "°C es igual a", temp_3_f, "°F")
