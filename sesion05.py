10f4fa947cb8755d6536a935e87abae1fb1c7f8e
Valentin Espinoza <valentinespinoza.ro@gmail.com>
2024-04-30 20:31:28 -0400

    Clase sesión 5

---
+++ sesion05.py
@@ -0,0 +1,90 @@
+print(type(1))
+
+print (10)
+print ( type (10) )
+
+variable = 100
+print (variable)
+print ( type (variable) )
+
+# Variable 20 Entero
+variable_2 = int (20)
+print (variable_2)
+print ( type (variable_2) )
+
+# Valor 10 en base decimal
+print ("Base decimal")
+print (10)
+# Valor 10 en binario
+print ("Base binaria")
+print (0b1010)
+# Valor 10 en octal
+print ("Base octal")
+print (0o12)
+# Valor 10 en hexadecimal
+print ("Base hexadecimal")
+print (0xa)
+
+# Entero con 60 dígitos
+variable_3 = 123456789012345678901234567890123456789012345678901234567890
+print (variable_3)
+print ( type (variable_3) )
+
+# Valor 0.5 Flotante
+print (0.5)
+print ( type (0.5) )
+
+# Variable 0.100546 Flotante
+variable_4 = 0.100546
+print (variable_4)
+print ( type (variable_4) )
+# Variable 1 Flotante
+variable_7 = float (1)
+print(variable_7)
+print ( type (variable_7) )
+
+# Precisión de 17 decimales
+variable_5 = 0.9999999999999999
+print(variable_5)
+print ( type (variable_5) )
+
+# Valor 2.0e-3 Flotante
+variable_6 = 2.0e-3
+print(variable_6)
+print ( type (variable_6) )
+
+# Operadores aritméticos
+a = 10
+b = 3
+# Suma
+print ("Suma")
+print (a + b)
+
+print ("División")
+print (a / b)
+
+# Potencia
+print ("Potencia")
+print (a ** b)
+
+# Módulo o residuo
+print ("Módulo o residuo")
+print (a % b)
+
+print ("División entera")
+print (a // b)
+
+#Un contador tiene 300 minutos y se le suman 3600 segundos, ¿Cuántas horas en total son?
+print(300/60 + 3600/60/60)
+
+minutos = 300
+tiempo_extra_segundos = 3600
+horas = (minutos + tiempo_extra_segundos / 60) / 60
+print (horas)
+
+#Numero par
+num_1 = 10
+num_2 = 5
+
+print(num_1 % num_2 == 0)
+
