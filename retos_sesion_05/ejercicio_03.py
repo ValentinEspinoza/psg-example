ec7ecf86d4d1d80b2b8d62affdce29ee60cb7902
Valentin Espinoza <valentinespinoza.ro@gmail.com>
2024-04-30 23:41:40 -0400

    Retos ejercicio 3

---
+++ retos_sesion_05/ejercicio_03.py
@@ -0,0 +1,11 @@
+# Conversiot a días, horas, minutos
+
+segundos = 288325
+
+minutos = segundos / 60
+horas = minutos / 60
+dias = horas / 24
+
+print(segundos,"segundos es igual a", minutos, "minutos")
+print(segundos,"segundos es igual a", horas, "horas")
+print(segundos,"segundos es igual a", dias, "días")
